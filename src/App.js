d15bf8392da78235808c6129e079a39aad97f19a
Ruan Reis <ruancosme23@gmail.com>
2023-03-10 01:52:55 -0300

    Inicio das aulas de React

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>HELLO WORLDs</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Iniciando os estudos de React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
